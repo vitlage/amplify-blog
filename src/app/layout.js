60fc550edb39454c12a5e6d53ad059cd9969fdd0
Vitalii Hladkyi <vitalijgladkij@gmail.com>
2025-03-13 21:23:12 +0100

    Changed favicon from default

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Convertic AI',
-  description: 'Convertic AI Blog',
+  description: 'Convertic AI',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
